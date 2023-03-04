6e560a9ef4d02f453dbe1263837cfc0148045dbd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-04 04:58:12 -0500

    more conversion to comprehensions and walrus operation

--- Item38.py
+++ Item38.py
@@ -1,28 +1,31 @@
 # Item 38: Accept Functions Instead of Classes for Simple Interfaces
-names = ['Socrates', 'Archimedes', 'Plato', 'Aristotle']
+names = ["Socrates", "Archimedes", "Plato", "Aristotle"]
 names.sort(key=len)
 print(names)
 
 
 def log_missing():
-    print('key added')  # if we do not increment at insertion, then it doesn't even fire off.
+    print(
+        "key added"
+    )  # if we do not increment at insertion, then it doesn't even fire off.
     return 0
 
 
 from collections import defaultdict
 
-current = {'green': 12, 'blue': 3}
+current = {"green": 12, "blue": 3}
 increments = [
-    ('red', 5),
-    ('blue', 17),
-    ('orange', 9),
+    ("red", 5),
+    ("blue", 17),
+    ("orange", 9),
 ]
 result = defaultdict(log_missing, current)
-print(f'before: {dict(result)}')
+print(f"before: {dict(result)}")
 for key, value in increments:
     result[key] += value
 
-print(f'after: {dict(result)}')
+print(f"after: {dict(result)}")
+
 
 # If I want to know the total number of keys that were missing, we can do that with a
 # stateful closure (Item 21).
@@ -40,10 +43,13 @@ def increment_with_reports(current, increments):
 
     return result, added_count
 
+
 result, count = increment_with_reports(current, increments)
 assert count == 2
 
+
 # Stateful closures tend to be harder to read, another approach is defining a small class to encapsulate the state
+
 
 class CountMissing:
     def __init__(self):
@@ -53,6 +59,7 @@ class CountMissing:
         self.added += 1
         return 0
 
+
 counter = CountMissing()
 result = defaultdict(counter.missing, current)  # Method reference
 for key, amount in increments:
@@ -60,9 +67,11 @@ for key, amount in increments:
 
 assert counter.added == 2
 
+
 # With classes, it is clearer, but it might not be as clear as we would like it.
 # Perhaps making a class that is callable will inform the client the intent and
 # How to use our class
+
 
 class BetterCountMissing:
     def __iter__(self):
@@ -72,12 +81,67 @@ class BetterCountMissing:
         self.added += 1
         return 0
 
+
 counter = BetterCountMissing()
-assert counter() == 0
-assert callable(counter)
+# assert counter() == 0
+# assert callable(counter)
 
-result = defaultdict(counter, current) # Relies on __call__
-for key, amount in increments:
-    result[key] = amount
+# result = defaultdict(counter, current)  # Relies on __call__
+# for key, amount in increments:
+#     result[key] = amount
+#
+# assert counter.added == 2
 
-assert counter.added == 2
+# Separately
+
+from typing import Iterable, Set
+import re
+
+ARN_REGEX = "(?P<arn>\w+):(?P<partition>\w+)\:(?P<service>\w+)?\:(?P<region>\w+)\:(?P<account_id>\w+)\:(?P<resource_id>\w+)"
+
+
+def collect_account_ids_from_arns(arns: Iterable[str]) -> Set[str]:
+    """Given several ARNS in the form arn:partition:service:region:account-id:resource-id
+    collect the unique account IDs found on those strings, and return them
+    """
+    collected_account_ids = set()
+    for arn in arns:
+        matched = re.match(ARN_REGEX, arn)
+        print(f"matched: {matched}")
+        if matched is not None:
+            account_id = matched.groupdict()["account_id"]
+            collected_account_ids.add(account_id)
+    return collected_account_ids
+
+
+# print(
+#     collect_account_ids_from_arns(
+#         [
+#             "asd1:partitasdasion1:asd1:region1:account1:resource1",
+#             "asd2:partitasdasion2:asd1:region2:account2:resource2",
+#         ]
+#     )
+# )
+
+
+def collect_account_ids_from_arns2(arns: Iterable[str]) -> Set[str]:
+    matched = filter(None, (re.match(ARN_REGEX, arn) for arn in arns))
+    return {m.groupdict()["account_id"] for m in matched}
+
+
+print(
+    collect_account_ids_from_arns2(
+        [
+            "asd1:partitasdasion1:asd1:region1:account1:resource1",
+            "asd2:partitasdasion2:asd1:region2:account2:resource2",
+        ]
+    )
+)
+
+
+def collect_account_ids_from_arns3(arns: Iterable[str]) -> Set[str]:
+    return {
+        matched.groupdict()["account_id"]
+        for arn in arns
+        if (matched := re.match(ARN_REGEX, arn)) is not None
+    }
